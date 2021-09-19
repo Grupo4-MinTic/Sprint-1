6109c492e0158bdfa091e6dd89a7ac98b0ed38ee
SantiagoD45 <90473093+SantiagoD45@users.noreply.github.com>
2021-09-19 17:19:26 -0500

    Add files via upload

---
+++ TiendaGenerica/src/main/java/com/BO/login/LoginApplication.java
@@ -0,0 +1,14 @@
+package com.BO.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Este es un mensaje nuevo");
+		SpringApplication.run(LoginApplication.class, args);
+	}
+
+}
